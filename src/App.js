f2f59d16e5503547412e795bc964f762c3f0cbb7
Kevin McNerney <kevin.m.mcnerney@gmail.com>
2023-10-02 19:53:54 -0400

    try catch

--- src/App.js
+++ src/App.js
@@ -48,8 +48,13 @@ const SCORES_ENDPOINT = 'https://guillotine-api-9268ebd959e7.herokuapp.com/live-
 
 
 async function getLiveScores(code) {
-  const scores = await axios.get('http://localhost:3001/live-projections')
-  return scores.data
+  try {
+    const scores = await axios.get(SCORES_ENDPOINT)
+    return scores.data
+  } catch (e) {
+    console.error('failed to get scores', e)
+    return []
+  }
 }
 
 
